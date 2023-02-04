1c2711e65916f6c1c188c77d22e00fcdc8c07936
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-04 13:24:49 +0600

    tailwind css added

--- app/page.js
+++ app/page.js
@@ -4,5 +4,5 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>Hello there</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
