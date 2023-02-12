be9ff116ada67a8f7a2ba123aacbb85262a097b8
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-12 23:02:12 +0600

    http methods added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,29 @@
 import databaseConnect from '@/db/utils/dbConnect';
 
 export default async function handler(req, res) {
-  await databaseConnect();
-  res.status(200).json({ name: 'John Doe' });
+  await databaseConnect().catch(() =>
+    res.status(405).json({ error: 'Error in the Connection' })
+  );
+
+  // type of request
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      res.status(200).json({ method, name: 'GET Request' });
+      break;
+    case 'POST':
+      res.status(200).json({ method, name: 'POST Request' });
+      break;
+    case 'PUT':
+      res.status(200).json({ method, name: 'PUT Request' });
+      break;
+    case 'DELETE':
+      res.status(200).json({ method, name: 'DELETE Request' });
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowd`);
+      break;
+  }
 }
