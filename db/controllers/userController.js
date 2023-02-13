b7a1ece19693f7e4b36fc3246b01f8aefa5b8b26
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-13 11:03:42 +0600

    user controller added

---
+++ db/controllers/userController.js
@@ -0,0 +1,12 @@
+import User from '../models/User';
+
+export async function getUsers(req, res) {
+  try {
+    const users = await User.find({});
+
+    if (!users) return res.status(404).json({ error: 'Data not Found' });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ error: 'Error While Fetching Data' });
+  }
+}
